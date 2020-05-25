af358efe659bfd10fad09e3f4ad67bd865b82c9f
mdobbles@yahoo.com <mdobbles@yahoo.com>
2020-05-25 11:52:20 -0700

    Initial pypy release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="community-transformers",
-    version="0.0.2",
+    version="0.0.1",
     author="Mike Dobbles",
     author_email="mdobbles@yahoo.com",
     description="A package of custom pyspark.ml transformers",
