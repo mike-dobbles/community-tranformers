8c0c6e519ae92ec7ef5b0f4d8def5aa798323f6f
mdobbles@yahoo.com <mdobbles@yahoo.com>
2020-05-25 11:50:58 -0700

    Updated unit tests

--- tests/test_t.py
+++ tests/test_t.py
@@ -1,6 +1,8 @@
 import pytest
-from transformers import t as tok
+import transformers.t as ct
 from pyspark.sql import SparkSession
+from mlflow.spark import save_model, load_model
+from pyspark.ml import Pipeline, PipelineModel
 
 # This runs before the tests and creates objects that can be used by the tests
 @pytest.fixture
@@ -21,9 +23,31 @@ def simple_test_dataframe():
 def test__NLTKWordPunctTokenizer(simple_test_dataframe):
 
     # Create the transformer
-    transformer = tok.NLTKWordPunctTokenizer(inputCol="sentence", outputCol="words", stopwords=['are', 'I'])
+    transformer = ct.NLTKWordPunctTokenizer(inputCol="sentence", outputCol="words", stopwords=['are', 'I'])
     df_transformed = transformer.transform(simple_test_dataframe)
-    df_transformed.show()
+
+    # Create a pipeline from the transformer
+    pipeline = Pipeline(stages=[transformer])
+
+    # fit the test data (which also builds the pipeline)
+    model = pipeline.fit(simple_test_dataframe)
+
+    # Test the pipeline
+    df_original_transformed = model.transform(simple_test_dataframe)
+
+    # Log the model and performance
+    save_model(model, "unit_test_model")
+    retrieved_model = load_model("unit_test_model")
+    df_retreived_transformed = retrieved_model.transform(simple_test_dataframe)
+
+    # Assert the retrieved model give the same results as the saved model
+    rows_in_common = df_original_transformed.intersect(df_retreived_transformed).count()
+    assert (df_original_transformed.count() == rows_in_common)
+
+    # Print results for visual inspection
+    print("\n")
+    print("test__NLTKWordPunctTokenizer: The following should show sentences broken into words")
+    df_retreived_transformed.select('sentence','words').show()
 
     # If we make it this far without crashing we pass (plus I'm visually reviewing results)
     assert True
