79e336d4388fd6d314f24e9082e5698b7695d043
mdobbles@yahoo.com <mdobbles@yahoo.com>
2020-05-25 09:57:27 -0700

    Changed unittests to pytest

--- tests/test_t.py
+++ tests/test_t.py
@@ -2,15 +2,28 @@ import pytest
 from transformers import t as tok
 from pyspark.sql import SparkSession
 
-def test__first_test():
-    tokenizer = tok.NLTKWordPunctTokenizer(inputCol="sentence", outputCol="words", stopwords=['are', 'I'])
+# This runs before the tests and creates objects that can be used by the tests
+@pytest.fixture
+def simple_test_dataframe():
+    """This is a simple dataframe for test use"""
+    # get a reference to spark
     spark = SparkSession.builder.getOrCreate()
-    sentenceDataFrame = spark.createDataFrame([
+
+    # create a test data frame
+    simple_df = spark.createDataFrame([
         (0, "Hi I heard about Spark"),
         (0, "I wish Java could use case classes"),
         (1, "Logistic regression models are neat")
     ], ["label", "sentence"])
 
-    df_transformed = tokenizer.transform(sentenceDataFrame)
+    return simple_df
+
+def test__NLTKWordPunctTokenizer(simple_test_dataframe):
+
+    # Create the transformer
+    transformer = tok.NLTKWordPunctTokenizer(inputCol="sentence", outputCol="words", stopwords=['are', 'I'])
+    df_transformed = transformer.transform(simple_test_dataframe)
     df_transformed.show()
+
+    # If we make it this far without crashing we pass (plus I'm visually reviewing results)
     assert True
