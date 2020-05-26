37f4bc593598d5e07f5044da192b157298da4409
mdobbles@yahoo.com <mdobbles@yahoo.com>
2020-05-25 22:04:32 -0700

    Added RegexSubstituter and TokenSubstituter

--- tests/test_t.py
+++ tests/test_t.py
@@ -3,27 +3,45 @@ import transformers.t as ct
 from pyspark.sql import SparkSession
 from mlflow.spark import save_model, load_model
 from pyspark.ml import Pipeline
+import os
+import pandas as pd
+
 
 # This runs before the tests and creates objects that can be used by the tests
 @pytest.fixture
 def simple_test_dataframe():
     """This is a simple dataframe for test use"""
+
     # get a reference to spark
     spark = SparkSession.builder.getOrCreate()
 
     # create a test data frame
-    simple_df = spark.createDataFrame([
-        (0, "Hi I heard about Spark"),
-        (0, "I wish Java could use case classes"),
-        (1, "Logistic regression models are neat")
-    ], ["label", "sentence"])
+    pdf = pd.DataFrame(columns=['text'],
+                 data=["This sentence ends with br sever blah blah stenosis regurgitatian<br>This sentence also ends with br<br>But this one doesn't",
+                       "Some sentences run together.The previous was an example",
+                       "Logistic regression models are neat.  This is a normal two sentence example."
+                       ])
 
-    return simple_df
+    return spark.createDataFrame(pdf)
+
+@pytest.fixture
+def numbers_dataframe():
+    """This is a dataframe filled with text of numbers for test use"""
+
+    # get a reference to spark
+    spark = SparkSession.builder.getOrCreate()
+
+    # create a test data frame
+    pdf = pd.DataFrame(columns=['text'],
+                 data=["onethousand two three four five",
+                       "six seven eight nine eight-hundred-ninetyfive"
+                       ])
+    return spark.createDataFrame(pdf)
 
 def test__NLTKWordPunctTokenizer(simple_test_dataframe):
 
     # Create the transformer
-    transformer = ct.NLTKWordPunctTokenizer(inputCol="sentence", outputCol="words", stopwords=['are', 'I'])
+    transformer = ct.NLTKWordPunctTokenizer(inputCol="text", outputCol="words", stopwords=['are', 'I'])
     df_transformed = transformer.transform(simple_test_dataframe)
 
     # Create a pipeline from the transformer
@@ -34,6 +52,11 @@ def test__NLTKWordPunctTokenizer(simple_test_dataframe):
 
     # Test the pipeline
     df_original_transformed = model.transform(simple_test_dataframe)
+
+    # Delete any previously save model (if it exists)
+    # (There may be a more elegant way to do this)
+    if os.path.exists("unit_test_model"):
+        os.system("rm -rf unit_test_model")
 
     # Log the model and performance
     save_model(model, "unit_test_model")
@@ -47,7 +70,96 @@ def test__NLTKWordPunctTokenizer(simple_test_dataframe):
     # Print results for visual inspection
     print("\n")
     print("test__NLTKWordPunctTokenizer: The following should show sentences broken into words")
-    df_retreived_transformed.select('sentence','words').show()
+    df_retreived_transformed.show(truncate=False)
+
+    # If we make it this far without crashing we pass (plus I'm visually reviewing results)
+    assert True
+
+def test__RegexSubstituter(simple_test_dataframe):
+
+    # Create the transformer
+    regexMatchers= ['(?<=[a-zA-Z])\.(?=[A-Z])',
+                    '<BR>',
+                    '<br>']
+    substitutions= ['. ',
+                    '. ',
+                    '. ']
+    transformer = ct.RegexSubstituter(inputCol="text", outputCol="regexcorrected",
+                                      regexMatchers=regexMatchers, substitutions=substitutions)
+
+    # Create a pipeline from the transformer
+    pipeline = Pipeline(stages=[transformer])
+
+    # fit the test data (which also builds the pipeline)
+    model = pipeline.fit(simple_test_dataframe)
+
+    # Test the pipeline
+    df_original_transformed = model.transform(simple_test_dataframe)
+
+    # Delete any previously save model (if it exists)
+    # (There may be a more elegant way to do this)
+    if os.path.exists("unit_test_model"):
+        os.system("rm -rf unit_test_model")
+
+    # Log the model and performance
+    save_model(model, "unit_test_model")
+    retrieved_model = load_model("unit_test_model")
+    df_retreived_transformed = retrieved_model.transform(simple_test_dataframe)
+
+    # Assert the retrieved model give the same results as the saved model
+    rows_in_common = df_original_transformed.intersect(df_retreived_transformed).count()
+    assert (df_original_transformed.count() == rows_in_common)
+
+    # Print results for visual inspection
+    print("\n")
+    print("test__RegexSubstituter: The following should show sentences broken into words")
+    df_retreived_transformed.show(truncate=False)
+
+    # If we make it this far without crashing we pass (plus I'm visually reviewing results)
+    assert True
+
+def test__TokenSubstituter(numbers_dataframe):
+
+    # Create the transformer
+    tokenizer = ct.NLTKWordPunctTokenizer(inputCol="text", outputCol="tokens")
+
+    # Create the transformer
+    tokenMatchers= ['two',
+                    'four',
+                    'nine']
+    substitutions= ['two-sub',
+                    'four-sub',
+                    'nine-sub']
+    toksub = ct.TokenSubstituter(inputCol="tokens", outputCol="swapped_tokens", tokenMatchers=tokenMatchers, substitutions=substitutions)
+
+    # Create a pipeline from the transformer
+    pipeline = Pipeline(stages=[tokenizer, toksub])
+
+
+    # fit the test data (which also builds the pipeline)
+    model = pipeline.fit(numbers_dataframe)
+
+    # Test the pipeline
+    df_original_transformed = model.transform(numbers_dataframe)
+
+    # Delete any previously save model (if it exists)
+    # (There may be a more elegant way to do this)
+    if os.path.exists("unit_test_model"):
+        os.system("rm -rf unit_test_model")
+
+    # Log the model and performance
+    save_model(model, "unit_test_model")
+    retrieved_model = load_model("unit_test_model")
+    df_retreived_transformed = retrieved_model.transform(numbers_dataframe)
+
+    # Assert the retrieved model give the same results as the saved model
+    rows_in_common = df_original_transformed.intersect(df_retreived_transformed).count()
+    assert (df_original_transformed.count() == rows_in_common)
+
+    # Print results for visual inspection
+    print("\n")
+    print("test__TokenSubstituter: two, four, and nine should be substituted")
+    df_retreived_transformed.show(truncate=False)
 
     # If we make it this far without crashing we pass (plus I'm visually reviewing results)
     assert True
--- transformers/t.py
+++ transformers/t.py
@@ -6,12 +6,14 @@ from pyspark.ml.param.shared import HasInputCol, HasOutputCol, Param, Params, Ty
 from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable, JavaMLReader, MLReadable, MLWritable
 from pyspark.sql.functions import udf
 from pyspark.sql.types import ArrayType, StringType
+import re
 
-class NLTKWordPunctTokenizer(
-    Transformer, HasInputCol, HasOutputCol,
-    # Credits https://stackoverflow.com/a/52467470
-    # by https://stackoverflow.com/users/234944/benjamin-manns
-    DefaultParamsReadable, DefaultParamsWritable, MLReadable, MLWritable):
+# Credits https://stackoverflow.com/a/52467470
+# by https://stackoverflow.com/users/234944/benjamin-manns
+
+class NLTKWordPunctTokenizer(Transformer, HasInputCol, HasOutputCol, DefaultParamsReadable, DefaultParamsWritable, MLReadable, MLWritable):
+    """This class uses nltk.tokenize.wordpunct_tokenize to generate an output column tranformation of a spark dataframe."""
+
     stopwords = Param(Params._dummy(), "stopwords", "stopwords",
                       typeConverter=TypeConverters.toListString)
 
@@ -60,4 +62,146 @@ class NLTKWordPunctTokenizer(
         t = ArrayType(StringType())
         out_col = self.getOutputCol()
         in_col = dataset[self.getInputCol()]
+        return dataset.withColumn(out_col, udf(f, t)(in_col))
+
+
+class RegexSubstituter(Transformer, HasInputCol, HasOutputCol, DefaultParamsReadable, DefaultParamsWritable, MLReadable,
+                       MLWritable):
+    """This class generates an output column tranformation of the input spark dataframe where the input column string
+    is searched with regex and a substitution is made where matches are found."""
+
+    regexMatchers = Param(Params._dummy(), "regexMatchers", "regexMatchers",
+                      typeConverter=TypeConverters.toListString)
+    substitutions = Param(Params._dummy(), "substitutions", "substitutions",
+                      typeConverter=TypeConverters.toListString)
+
+    @keyword_only
+    def __init__(self, inputCol=None, outputCol=None, regexMatchers=None, substitutions=None):
+        module = __import__("__main__")
+        setattr(module, 'RegexSubstituter', RegexSubstituter)
+        super(RegexSubstituter, self).__init__()
+        self.regexMatchers = Param(self, "regexMatchers", "")
+        self.substitutions = Param(self, "substitutions", "")
+        self._setDefault(regexMatchers=[],substitutions=[])
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    @keyword_only
+    def setParams(self, inputCol=None, outputCol=None, regexMatchers=None, substitutions=None):
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+
+    def setRegexMatchers(self, value):
+        self._paramMap[self.regexMatchers] = value
+        return self
+
+    def getRegexMatchers(self):
+        return self.getOrDefault(self.regexMatchers)
+
+    def setSubstitutions(self, value):
+        self._paramMap[self.substitutions] = value
+        return self
+
+    def getSubstitutions(self):
+        return self.getOrDefault(self.substitutions)
+
+    def _transform(self, dataset):
+        regexMatchers = self.getRegexMatchers()
+        substitutions = self.getSubstitutions()
+
+        # throw error if the regex patterns and substitution arrays don't match
+        if len(substitutions) != len(regexMatchers):
+            raise ValueError("regexMatchers and substitutions must be the same length")
+
+        # user defined function to loop through each of the substitutions and apply
+        # them to the passed text
+        t = StringType()
+
+        def f(text):
+            for idx, reg in enumerate(regexMatchers):
+                text = re.sub(reg, substitutions[idx], text)
+            return text
+
+        # Select the input column
+        in_col = dataset[self.getInputCol()]
+
+        # Get the name of the output column
+        out_col = self.getOutputCol()
+
+        return dataset.withColumn(out_col, udf(f, t)(in_col))
+
+class TokenSubstituter(Transformer, HasInputCol, HasOutputCol, DefaultParamsReadable, DefaultParamsWritable,
+                       MLReadable, MLWritable):
+    """This class expects the input column to have an array of string tokens.  It searches those tokens one by one
+    and replaces them with the substitutions if a match is found."""
+
+    tokenMatchers = Param(Params._dummy(), "tokenMatchers", "tokenMatchers",
+                          typeConverter=TypeConverters.toListString)
+    substitutions = Param(Params._dummy(), "substitutions", "substitutions",
+                          typeConverter=TypeConverters.toListString)
+
+    @keyword_only
+    def __init__(self, inputCol=None, outputCol=None, tokenMatchers=None, substitutions=None):
+        module = __import__("__main__")
+        setattr(module, 'TokenSubstituter', RegexSubstituter)
+        super(TokenSubstituter, self).__init__()
+        self.tokenMatchers = Param(self, "tokenMatchers", "")
+        self.substitutions = Param(self, "substitutions", "")
+        self._setDefault(tokenMatchers=[], substitutions=[])
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    @keyword_only
+    def setParams(self, inputCol=None, outputCol=None, tokenMatchers=None, substitutions=None):
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+
+    def setTokenMatchers(self, value):
+        self._paramMap[self.tokenMatchers] = value
+        return self
+
+    def getTokenMatchers(self):
+        return self.getOrDefault(self.tokenMatchers)
+
+    def setSubstitutions(self, value):
+        self._paramMap[self.substitutions] = value
+        return self
+
+    def getSubstitutions(self):
+        return self.getOrDefault(self.substitutions)
+
+    def _transform(self, dataset):
+        tokenMatchers = self.getTokenMatchers()
+        substitutions = self.getSubstitutions()
+
+        # throw error if the regex patterns and substitution arrays don't match
+        if len(substitutions) != len(tokenMatchers):
+            raise ValueError("tokenMatchers and substitutions must be the same length")
+
+        # user defined function to loop through each of the substitutions and apply
+        # them to the passed text
+        t = ArrayType(StringType())
+
+        def f(token_array):
+            # Cycle through the tokens in the passed column cell one by one
+            # If it matches a token in the tokenMatchers array, then swap on that token,
+            # otherwise, leave it alone
+            returned_array = []
+            for tok in token_array:
+
+                #See if we can find the token and if we do swap it for the token in the same position in subsitutions
+                try:
+                    idx = tokenMatchers.index(tok)
+                    returned_array.append(substitutions[idx])
+                except:
+                    returned_array.append(tok)
+
+            return returned_array
+
+        # Select the input column
+        in_col = dataset[self.getInputCol()]
+
+        # Get the name of the output column
+        out_col = self.getOutputCol()
+
         return dataset.withColumn(out_col, udf(f, t)(in_col))
