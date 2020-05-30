7f1139e30c5ac435c0d972c55c39aed2c6a58a79
mdobbles@yahoo.com <mdobbles@yahoo.com>
2020-05-29 18:10:34 -0700

    Removed unit test from package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="community-transformers",
-    version="0.0.4",
+    version="0.0.5",
     author="Mike Dobbles",
     author_email="mdobbles@yahoo.com",
     description="A package of custom pyspark.ml transformers",
